605106e5e21797e27363a9ee6808769c00da5902
Codyru <codruthristu@ymail.com>
2023-04-28 21:44:08 +0300

    Create Bottom Navigation Menu

--- ScannerDocumente/app/src/main/java/com/android/example/scannerdocumente/MainActivity.kt
+++ ScannerDocumente/app/src/main/java/com/android/example/scannerdocumente/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.android.example.scannerdocumente
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnItemReselectedListener {
+            when(it.itemId){
+                R.id.person-> startActivity(Intent(this,CameraActivity::class.java))
+
+            }
+            true
+        }
+    }
     }
 }

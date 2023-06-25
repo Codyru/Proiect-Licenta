b016e7c699c16e716ae407b877ae9fa43235d223
Codyru <codruthristu@ymail.com>
2023-06-26 01:04:53 +0300

    Add DiplomaPickerFragment functionality to extract text and data pass from RV to same fragment from item

--- ScannerDocumente/app/src/main/java/com/android/example/scannerdocumente/DiplomaPickerFragment.kt
+++ ScannerDocumente/app/src/main/java/com/android/example/scannerdocumente/DiplomaPickerFragment.kt
@@ -2,22 +2,71 @@ package com.android.example.scannerdocumente
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import org.w3c.dom.Text
 
 
 class DiplomaPickerFragment : Fragment() {
 
     private var pictureUri: Uri? = null
+    private lateinit var btnValidateDiploma: Button
+    private lateinit var ivDiploma: ImageView
+    private var documentType: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diploma_picker, container, false)
+        val view = inflater.inflate(R.layout.fragment_diploma_picker, container, false)
+
+        btnValidateDiploma = view.findViewById(R.id.btnValidateDiploma)
+        ivDiploma = view.findViewById(R.id.ivDiploma)
+
+        arguments?.let {
+            pictureUri = it.getParcelable("PICTURE_URI")
+            documentType = it.getString("DOCUMENT_TYPE")
+        }
+
+        btnValidateDiploma.setOnClickListener {
+            extractDiplomaText()
+        }
+
+        loadFromPictureFromRV()
+
+        return view
+    }
+
+    fun loadFromPictureFromRV(){
+        if(pictureUri != null && documentType == "Diploma") {
+            Glide.with(requireContext())
+                .load(pictureUri)
+                .into(ivDiploma)
+        }
+    }
+
+    fun extractDiplomaText(){
+        var recognizeDiploma = TextRecognition(requireContext())
+        pictureUri?.let { recognizeDiploma.recognizeText(it) { result ->
+            if (result.isNullOrBlank() || result.trim() == recognizeDiploma.errorString || result.length < 10){
+                Log.d("EROARE_RECUNOASTERE", "Nu am putut recunoaste textul.")
+                activity?.runOnUiThread {
+                    Toast.makeText(requireContext(), R.string.unrecognized_text, Toast.LENGTH_LONG).show()
+                }
+            }
+            else{
+                Log.d("RESULT", "$result")
+                Toast.makeText(requireContext(), "Rezultat recunoscut", Toast.LENGTH_LONG).show()
+            }
+        } }
     }
 
 }
